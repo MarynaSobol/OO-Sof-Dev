8fd9d57f80b85ebf656991c73d06087c66eeaf22
MarynaSobol <C00312784@setu.ie>
2025-10-13 12:25:29 +0100

    Lab3
    lab3

---
+++ BankDriver.java
@@ -0,0 +1,14 @@
+public class BankDriver 
+{
+    public static void main(String[] args)
+    {
+        SavingsAccount saver1 = new SavingsAccount();
+        saver1.setSavings Balance (2000.00);
+        SavingsAccount saver2 = new Savings Account(3000.00);
+        SavingsAccount.modifyInterest Rate(0.041);
+        saver1.calculate Monthlyinterest();
+        saver2.calculate Monthlyinterest();
+        System.out.println(saver1);
+        System.out.println(saver2.toString());
+    }
+}
